28df52f64fbed9622799eb9aca92fc5c22dbaf58
nadeemz81 <nadeemraza565@gmail.com>
2025-03-17 17:59:43 +0500

    again2

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -5,12 +5,12 @@ export default function Contact() {
     <section id="contact" className={styles.contact}>
      {/* Contact Section */}
      <div className="mt-8 text-center">
-        <h3 className="text-2xl font-semibold text-blue-400 mb-2">Contact Me</h3>
+        <h4 className="text-2xl font-semibold text-blue-400 mb-2">Contact Me</h4>
         <p className="text-gray-300 flex items-center justify-center gap-2">
           <FaEnvelope className="text-red-400" /> nadeemr1@yahoo.com
         </p>
         <p className="text-gray-300 flex items-center justify-center gap-2 mt-2">
-          <FaPhone className="text-blue-400" /> +92301-11007025
+          <FaPhone className="text-blue-400" /> +9231-11007025
         </p>
         <p className="text-gray-300 flex items-center justify-center gap-2 mt-2">
           <FaLinkedin className="text-blue-500" /> <a href="https://www.linkedin.com/in/nadeemraza-624454283/" className="hover:underline">LinkedIn Profile</a>
